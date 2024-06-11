2efd53cc21f1343140f3d32faddbddf47f29e381
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 22:28:32 +0900

    feat: product repository page 메소드 추가

--- src/main/java/io/whatap/whatap/domain/product/repository/ProductRepository.java
+++ src/main/java/io/whatap/whatap/domain/product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package io.whatap.whatap.domain.product.repository;
 
 import io.whatap.whatap.domain.product.Product;
+import io.whatap.whatap.domain.product.dto.ProductResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<ProductResponse> findAllById(Long id, Pageable pageable);
 }
