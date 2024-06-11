ad705928b58cefea00f819f04a5aa6fc2e32cba7
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 22:58:35 +0900

    add: update order request 추가

---
+++ src/main/java/io/whatap/whatap/domain/order/dto/UpdateOrderRequest.java
@@ -0,0 +1,17 @@
+package io.whatap.whatap.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateOrderRequest {
+    @Min(value = 0, message = "수량(quantity)은 0 이상으로 선택해주시길 바랍니다.")
+    @NotNull(message = "수량(quantity)이 null입니다.")
+    private Integer quantity;
+}
