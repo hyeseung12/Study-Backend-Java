60cb4830dd69a67afcfb3626b7b9bdadcb84cf7b
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 22:09:59 +0900

    feat: order service findAll() 추가

--- src/main/java/io/whatap/whatap/domain/order/service/OrderService.java
+++ src/main/java/io/whatap/whatap/domain/order/service/OrderService.java
@@ -7,6 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class OrderService {
@@ -15,5 +18,13 @@ public class OrderService {
     @Transactional
     public OrderResponse save(AddOrderRequest request) {
         return new OrderResponse(orderRepository.save(request.toEntity()));
+    }
+
+    @Transactional(readOnly = true)
+    public List<OrderResponse> findAll() {
+        return orderRepository.findAll()
+                .stream()
+                .map(OrderResponse::new)
+                .collect(Collectors.toList());
     }
 }
