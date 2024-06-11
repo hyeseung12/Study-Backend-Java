f1f012716a369f0d1e43c4163597d3dc7b0dca2d
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 23:07:16 +0900

    add: update product request 추가

---
+++ src/main/java/io/whatap/whatap/domain/product/dto/UpdateProductRequest.java
@@ -0,0 +1,24 @@
+package io.whatap.whatap.domain.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateProductRequest {
+    @NotEmpty(message = "상품 이름(name)을 작성해주세요.")
+    private String name;
+
+    @NotNull(message = "상품 설명(description)이 null입니다.")
+    private String description;
+
+    @NotNull(message = "상품 가격(price)이 null입니다.")
+    @Min(value = 0, message = "가격(price)은 0 이상으로 작성해주시길 바랍니다.")
+    private Integer price;
+}
