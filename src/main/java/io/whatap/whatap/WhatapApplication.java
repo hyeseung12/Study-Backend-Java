cc6cd9a58d5115c9df5d00e5f8b609abc7c79cdd
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 17:08:10 +0900

    feat: swagger api 명세서 적용

--- src/main/java/io/whatap/whatap/WhatapApplication.java
+++ src/main/java/io/whatap/whatap/WhatapApplication.java
@@ -2,8 +2,10 @@ package io.whatap.whatap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class WhatapApplication {
 
 	public static void main(String[] args) {
