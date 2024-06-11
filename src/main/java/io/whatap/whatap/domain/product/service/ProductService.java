6a89c6d79207c3dca5bb91f88bcb759c58efba93
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 19:46:42 +0900

    feat: product service findById() 추가

--- src/main/java/io/whatap/whatap/domain/product/service/ProductService.java
+++ src/main/java/io/whatap/whatap/domain/product/service/ProductService.java
@@ -1,7 +1,9 @@
 package io.whatap.whatap.domain.product.service;
 
+import io.whatap.whatap.domain.product.Product;
 import io.whatap.whatap.domain.product.dto.AddProductRequest;
 import io.whatap.whatap.domain.product.dto.ProductResponse;
+import io.whatap.whatap.domain.product.exception.ProductNotFoundException;
 import io.whatap.whatap.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,5 +17,13 @@ public class ProductService {
     @Transactional
     public ProductResponse save(AddProductRequest request) {
         return new ProductResponse(productRepository.save(request.toEntity()));
+    }
+
+    @Transactional(readOnly = true)
+    public ProductResponse findById(Long id) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> ProductNotFoundException.EXCEPTION);
+
+        return new ProductResponse(product);
     }
 }
