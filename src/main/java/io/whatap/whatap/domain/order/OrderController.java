c3881e89ffd4446014bebc7f651b88d52861cc1b
MsEmily1020 <peace10200khs@gmail.com>
2024-06-11 21:54:43 +0900

    feat: order controller orderProduct 추가

---
+++ src/main/java/io/whatap/whatap/domain/order/OrderController.java
@@ -0,0 +1,22 @@
+package io.whatap.whatap.domain.order;
+
+import io.whatap.whatap.domain.order.dto.AddOrderRequest;
+import io.whatap.whatap.domain.order.dto.OrderResponse;
+import io.whatap.whatap.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/order")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public OrderResponse orderProduct(@RequestBody AddOrderRequest request) {
+        return orderService.save(request);
+    }
+}
