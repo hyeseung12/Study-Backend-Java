233a93c2138a183da667e15f7e13204960b3b777
MsEmily1020 <peace10200khs@gmail.com>
2024-06-15 02:43:38 +0900

    add: AddProductRequest 추가

---
+++ product/src/main/java/io/whatap/product/dto/AddProductRequest.java
@@ -0,0 +1,38 @@
+package io.whatap.product.dto;
+
+import io.whatap.product.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddProductRequest {
+    @NotEmpty(message = "상품 이름(name)을 작성해주세요.")
+    private String name;
+
+    @NotNull(message = "상품 설명(description)이 null입니다.")
+    private String description;
+
+    @NotNull(message = "상품 가격(price)이 null입니다.")
+    @Min(value = 0, message = "가격(price)은 0 이상으로 작성해주시길 바랍니다.")
+    private Integer price;
+
+    @NotNull(message = "상품 재고량(inventory)이 null입니다.")
+    @Min(value = 0, message = "재고량(inventory)은 0 이상으로 작성해주시길 바랍니다.")
+    private Long inventory;
+
+    public Product toEntity() {
+        return Product.builder()
+                .name(name)
+                .description(description)
+                .price(price)
+                .inventory(inventory)
+                .build();
+    }
+}
