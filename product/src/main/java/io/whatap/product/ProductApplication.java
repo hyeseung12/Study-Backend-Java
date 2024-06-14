517bb4b08718e897a01a67293a7e2b8af0b8664a
MsEmily1020 <peace10200khs@gmail.com>
2024-06-15 02:33:12 +0900

    add: BaseTimeEntity 추가

--- product/src/main/java/io/whatap/product/ProductApplication.java
+++ product/src/main/java/io/whatap/product/ProductApplication.java
@@ -2,7 +2,11 @@ package io.whatap.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableEurekaClient
+@EnableJpaAuditing
 @SpringBootApplication
 public class ProductApplication {
 
