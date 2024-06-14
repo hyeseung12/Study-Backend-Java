40f6700df3473b10691939165c7c0b7c329a654e
MsEmily1020 <peace10200khs@gmail.com>
2024-06-15 02:49:52 +0900

    add: ProductRepository 추가

---
+++ product/src/main/java/io/whatap/product/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package io.whatap.product.repository;
+
+import io.whatap.product.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
