801da18eb1e9ee88f97876012142a399238bcfc9
MsEmily1020 <peace10200khs@gmail.com>
2024-06-15 22:07:08 +0900

    feat: feign client exception handler 추가

---
+++ order/src/main/java/io/whatap/order/global/exception/GlobalClientExceptionHandler.java
@@ -0,0 +1,31 @@
+package io.whatap.order.global.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalClientExceptionHandler {
+    private final ObjectMapper objectMapper;   //🔥🔥
+
+    /**
+     * FeignClient에서 발생한 예외 그대로 전달🔥🔥
+     */
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity feignExceptionHandler(FeignException feignException) throws JsonProcessingException {
+
+        String responseJson = feignException.contentUTF8();
+        Map<String, String> responseMap = objectMapper.readValue(responseJson, Map.class);
+
+        return ResponseEntity
+                .status(feignException.status())
+                .body(responseMap);
+    }
+}
